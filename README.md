8d79e71fabc369763c13cb098620c54224135b99
sbhalaru <97138710+sbhalaru@users.noreply.github.com>
2022-03-03 15:51:37 -0800

    Preprocessing Function and Readme
    Adding information to Readme and creating the Preprocessing function from the jupyter notebook. Feel free to edit and make changes.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Solar-Flare-Prediction
+# Solar-Flare-Prediction README
+**Version 1.0.0**
+
+preprocessing.py
+This file contains the follwing 2 functions:
+parse_date(sdatex, stimex):
+This is used to parse the strings from the date time data in the dataframe.
+This allows us to create a more robust datetime format for its start, peak, and end.
+
+preprocess(fname):
+This allows us to read in the csv file containing the whole solar flare data that we need to use for the project.
+We drop certain columns and values that are no use to our project goal and helps to save time when processing the dataframe.
+We also make certain changes to the datetime formats given so that they become easier to handle in the machine learning section.
+Along with the datetime, we changes values of the energy which is our main focus. Since they are in a range format,
+using machine learning makes it extremely complicated, therefore we converted them into numbers from 0 to 8 and will reconvert after the machine learning process
+We also drop certain unwanted columns at the end and change the duration column to a logarithmic scale since it is slightly skewed.
+
+
+
+---
+## Authors
+Group 5:
+Haaris Rahman, Kevin Mills, Shaan Bhalaru, Yongxing Chen, Shusen Lin
+
+
+
---
+++ preprocesssing.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+
+
+def parse_date(sdatex, stimex):
+    """Goes through our string date data and strips them to create a fluid datetime setup for each time a solar flare starts, peaks, and ends
+
+    Args:
+        sdatex (str): contains the date in a string format
+        stimex (str): contains the time in a string format
+
+    Returns:
+        datetime:  creates a datetime type data that is easy to use for our uses
+    """   
+
+    datex = datetime.strptime(sdatex, '%Y-%m-%d')
+    timex = datetime.strptime(stimex, '%H:%M:%S')
+    return datetime(datex.year,datex.month,datex.day,timex.hour,timex.minute,timex.second)
+
+
+def preprocess(fname):
+    """This preprocesses our data by going through and getting rid of values and modifying them to allow for a smooth machine learning process
+
+    Args:
+        fname (str): the name of the csv file containing our solar flare information
+
+    Returns:
+        solar_df (pandas.core.frame.DataFrame): the data frame that contains solar flare data after processings
+    """    
+    solar_df = pd.read_csv(fname, sep=',', index_col=0)
+
+    ''' Drop the flag columns'''
+    solar_df = solar_df.drop(["active.region.ar", "flag.1", "flag.2", "flag.3", "flag.4", "flag.5"], axis=1)
+
+    ''' Some Radial Values are beyond 2000'''
+    solar_df = solar_df[solar_df["x.pos.asec"] > -2000]
+
+
+
+
+    # Adding year, month, day, start date, peak date, end date and dropping earlier columns
+    solar_df['dt.start'] = solar_df[['start.date','start.time']].apply(lambda x: parse_date(x[0],x[1]), axis=1)
+    solar_df['dt.peak'] = solar_df[['start.date','peak']].apply(lambda x: parse_date(x[0],x[1]), axis=1)
+    solar_df['dt.end'] = solar_df[['start.date','end']].apply(lambda x: parse_date(x[0],x[1]), axis=1)
+
+    solar_df.drop(['start.date','start.time','peak','end'], axis=1, inplace=True)
+
+    # add new columns
+    solar_df['year'] = solar_df['dt.start'].apply(lambda col: col.year)
+    solar_df['month'] = solar_df['dt.start'].apply(lambda col: col.month)
+    solar_df['day'] = solar_df['dt.start'].apply(lambda col: col.day)
+
+
+    solar_df = solar_df.rename(columns={'duration.s': 'duration', 'peak.c/s': 'peak_c_s', 'total.counts': 'total_counts', 
+                                    'energy.kev': 'energy_kev', 'x.pos.asec': 'x_pos', 'y.pos.asec': 'y_pos', 
+                                    'dt.start': 'date_start', 'dt.peak':'date_peak', 'dt.end': 'date_end'})
+
+
+
+    # Enumerating energy range values from str to category
+    category = {'3-6': 0, '6-12': 1, '12-25': 2, '25-50': 3, '50-100': 4, '100-300': 5, '300-800': 6, '800-7000': 7, '7000-20000': 8}
+    solar_df['energy_kev'] = solar_df['energy_kev'].map(category)
+
+    solar_df['duration'] = np.log1p(solar_df['duration'])
+
+    solar_df = solar_df.drop(['date_start', 'date_peak', 'date_end'], axis=1)
+
+    
+    return solar_df
+
+
